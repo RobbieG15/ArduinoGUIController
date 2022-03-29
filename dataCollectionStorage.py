626c6adfee23139ac34f01c13e9c80c5a4256f85
Robert Greenslade <robbieg1515@gmail.com>
2022-03-29 00:23:27 -0400

    Added: functionality for creating presets of arduino setups. getPresetNames() is unfinished.

--- dataCollectionStorage.py
+++ dataCollectionStorage.py
@@ -1,3 +1,4 @@
+import readline
 from serial.tools.list_ports import comports
 
 def getPorts():
@@ -6,3 +7,42 @@ def getPorts():
     for port in availablePorts:
         portNames.append(port[0])
     return portNames
+
+def getPresetNames():
+    with open('presets.txt') as presets:
+        presetNameList = []
+        i = 0
+        while i < len(presets.readlines()):
+            if i % 4 == 0:
+                presetNameList.append(presets.readline().strip())
+            else:
+                presets.readline()
+            i+=1
+    print(presetNameList)
+
+
+class Presets:
+    def __init__(self):
+        self.presetName = ''
+        self.board = ''
+        self.port = ''
+        self.pins = [] # pins take form [name, d/a, num, (i/o/p/s)]
+
+    def to_string(self):
+        print(f'Preset: {self.presetName}\n')
+        print(f'Board: {self.board}\n')
+        print(f'Port: {self.port}\n')
+        for pin in self.pins:
+            print(f'{pin[0]} pin has settings as follows: type of {pin[1]}, pin num of {pin[2]}, and is in {pin[3]} mode.\n')
+
+    def save(self):
+        with open('presets.txt', 'a') as presets:
+            presets.write(f'{self.presetName}\n')
+            presets.write(f'{self.board}\n')
+            presets.write(f'{self.port}\n')
+            for pin in self.pins:
+                if self.pins.index(pin) == (len(self.pins) - 1):
+                    presets.write(f'{pin}\n')
+                else:
+                    presets.write(f'{pin},')
+
--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 # imports
 from tkinter import *
 from tkinter import ttk
+from turtle import clear
 import dataCollectionStorage
 import controller
 
@@ -13,91 +14,113 @@ frm = ttk.Frame(root, padding = 10)
 frm.grid()
 
 # gui functions and variables needed
+def clearFrame():
+   for widgets in frm.winfo_children():
+      widgets.destroy()
 
-def destroyWidgets(widgets):
-    for widget in widgets:
-        widget.destroy()
+def collectBoardAndPort(board, port, presetName):
+    preset.presetName = presetName
+    preset.board = board
+    preset.port = port
+    clearFrame()
+    header()
+    addPin()
 
-def collectBoardAndPort(board, port):
-    try:
-        #controller.connect(board, port)
-        destroyWidgets([boardDropDown, portDropDown, confirmBoardAndPort, boardText, portText])
-        addPin()
-    except:
-        print('Port selection was wrong or board type was wrong. Change and resubmit.')
+def saveAndAdd(pinNum, pinType, pinMode, pinName):
+    preset.pins.append((pinName, pinType, pinNum, pinMode))
+    clearFrame()
+    header()
+    addPin()
 
-def displayPinFail():
-    pass
+def saveAndQuit(pinNum, pinType, pinMode, pinName):
+    preset.pins.append((pinName, pinType, pinNum, pinMode))
+    preset.save()
+    clearFrame()
+    mainMenu()
 
-def saveAndAdd(pinNum, pinType, pinMode):
-    passed = controller.checkPin(pinNum, pinType, pinMode)
-    if not passed:
-        displayPinFail()
-        addPin()
-def saveAndQuit(pinNum, pinType, pinMode):
-    passed = controller.checkPin(pinNum, pinType, pinMode)
-    if not passed:
-        displayPinFail()
-        addPin()
 
 # gui contents
-# header
-title = ttk.Label(frm, text = 'Arduino I/O Controller')
-title.grid(row=0, column=0, columnspan=2)
+def selectNameBoardPort():
+    # get rid of main menu options
+    clearFrame()
+    header()
+    # new preset from data collection
+    global preset 
+    preset = dataCollectionStorage.Presets()
+    # preset name
+    presetNameText = ttk.Label(frm, text = 'Preset Name:')
+    presetNameText.grid(row=1, column=0, columnspan=2)
+    presetName = ttk.Entry(frm)
+    presetName.grid(row=1, column=3)
+    # drop down board type
+    boards = ['Arduino', 'ArduinoMega']
+    boardType = StringVar(root)
+    boardType.set(boards[0])
+    boardText = ttk.Label(frm, text = 'Board Type:')
+    boardText.grid(row=2, column=0, columnspan=2)
+    boardDropDown = OptionMenu(frm, boardType, *boards)
+    boardDropDown.grid(row=2, column=3)
+
+    # drop down port selection
+    ports = dataCollectionStorage.getPorts()
+    portType = StringVar(root)
+    portType.set(ports[0])
+    portText = ttk.Label(frm, text = 'Port Type:')
+    portText.grid(row=3, column=0, columnspan=2)
+    portDropDown = OptionMenu(frm, portType, *ports)
+    portDropDown.grid(row=3, column=3)
 
 
-# exit button
-exitButton = ttk.Button(frm, text="Quit", command=root.destroy)
-exitButton.grid(row=0, column=3)
-
-# drop down board type
-boards = ['Arduino', 'ArduinoMega']
-boardType = StringVar(root)
-boardType.set(boards[0])
-boardText = ttk.Label(frm, text = 'Board Type:')
-boardText.grid(row=1, column=0, columnspan=2)
-boardDropDown = OptionMenu(frm, boardType, *boards)
-boardDropDown.grid(row=1, column=3)
-
-# drop down port selection
-ports = dataCollectionStorage.getPorts()
-portType = StringVar(root)
-portType.set(ports[0])
-portText = ttk.Label(frm, text = 'Port Type:')
-portText.grid(row=2, column=0, columnspan=2)
-portDropDown = OptionMenu(frm, portType, *ports)
-portDropDown.grid(row=2, column=3)
-
-
-# confirm board and port button
-confirmBoardAndPort = Button(frm, text='Done', command=lambda: collectBoardAndPort(boardType.get(), portType.get()))
-confirmBoardAndPort.grid(row=3, column=0, columnspan=4, sticky=E)
+    # confirm board and port button
+    confirmBoardAndPort = Button(frm, text='Done', command=lambda: collectBoardAndPort(boardType.get(), portType.get(), presetName.get()))
+    confirmBoardAndPort.grid(row=4, column=0, columnspan=4, sticky=E)
 
 # adding pins
-
 def addPin():
     col = 0
     row = 1
+    pinNameText = ttk.Label(frm, text = 'Pin Name:')
+    pinNameText.grid(column=col, row=row)
+    pinName = ttk.Entry(frm)
+    pinName.grid(column=col+1, row=row)
     pinNumText = ttk.Label(frm, text = 'Pin Number:')
-    pinNumText.grid(column=col, row=row)
+    pinNumText.grid(column=col, row=row+1)
     pinNum = IntVar(root)
     pinNumDropDown = OptionMenu(frm, pinNum, *[item for item in range(1,21)])
-    pinNumDropDown.grid(column=col+1, row=row)
-    pinTypeText = ttk.Label(frm, text='Pin Type:')
-    pinTypeText.grid(column=col, row=row+1)
+    pinNumDropDown.grid(column=col+1, row=row+1)
+    pinTypeText = ttk.Label(frm, text='Pin Type (Analog or Digital):')
+    pinTypeText.grid(column=col, row=row+2)
     pinType = StringVar(root)
-    pinTypeDropDown = OptionMenu(frm, pinType, *['Analog', 'Digital'])
-    pinTypeDropDown.grid(column=col+1, row=row+1)
-    pinModeText = ttk.Label(frm, text='Pin Mode:')
-    pinModeText.grid(column=col, row=row+2)
+    pinTypeDropDown = OptionMenu(frm, pinType, *['a', 'd'])
+    pinTypeDropDown.grid(column=col+1, row=row+2)
+    pinModeText = ttk.Label(frm, text='Pin Mode (Input, Output, PWM, Servo):')
+    pinModeText.grid(column=col, row=row+3)
     pinMode = StringVar(root)
-    pinModeDropDown = OptionMenu(frm, pinMode, *['Input', 'Output', 'PWM', 'Servo'])
-    pinModeDropDown.grid(column=col+1, row=row+2)
-    addButton1 = ttk.Button(frm, text='Add and Continue', command=lambda: saveAndAdd(pinNum.get(), pinType.get(), pinMode.get()))
-    addButton1.grid(column=col, row=row+3)
-    addButton2 = ttk.Button(frm, text='Add and Quit', command=lambda: saveAndQuit(pinNum.get(), pinType.get(), pinMode.get()))
-    addButton2.grid(column=col+1, row=row+3)
+    pinModeDropDown = OptionMenu(frm, pinMode, *['i', 'o', 'p', 's'])
+    pinModeDropDown.grid(column=col+1, row=row+3)
+    addButton1 = ttk.Button(frm, text='Add and Continue', command=lambda: saveAndAdd(pinNum.get(), pinType.get(), pinMode.get(), pinName.get()))
+    addButton1.grid(column=col, row=row+4)
+    addButton2 = ttk.Button(frm, text='Add and Quit', command=lambda: saveAndQuit(pinNum.get(), pinType.get(), pinMode.get(), pinName.get()))
+    addButton2.grid(column=col+1, row=row+4)
+
+def header():
+    # header
+    title = ttk.Label(frm, text = 'Arduino I/O Controller')
+    title.grid(row=0, column=0, columnspan=2, sticky=W)
+    # exit button
+    exitButton = ttk.Button(frm, text="Quit", command=root.destroy)
+    exitButton.grid(row=0, column=3, sticky=E)
+
+def mainMenu():
+    header()
+    # new preset button
+    newPresetButton = ttk.Button(frm, text="New Preset", command=selectNameBoardPort)
+    newPresetButton.grid(row=1, column=0)
+
+
 
 
 # running main gui loop
+mainMenu()
 root.mainloop()
+dataCollectionStorage.getPresetNames()
