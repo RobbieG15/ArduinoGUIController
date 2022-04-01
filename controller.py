aac3af43a2b89dceb1e25f8765d9080a94c7bb76
RobbieG15 <robbieg1515@gmail.com>
2022-04-01 14:29:09 -0400

    Update controller.py
    Started to implement operating the preset.

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 from pyfirmata import *
 import ast
 from dataCollectionStorage import boardTypes
+import keyboard
 
 class RunPreset:
     def __init__(self):
@@ -26,16 +27,21 @@ class RunPreset:
         try:
             if self.board == boardTypes[0]:
                 board = Arduino(self.port)
+                print(f'Established connection to {self.board} on port {self.port}.')
             elif self.board == boardTypes[1]:
                 board = ArduinoMega(self.port)
+                print(f'Established connection to {self.board} on port {self.port}.')
             elif self.board == boardTypes[2]:
                 board = ArduinoDue(self.port)
+                print(f'Established connection to {self.board} on port {self.port}.')
             elif self.board == boardTypes[3]:
                 board = ArduinoNano(self.port)
+                print(f'Established connection to {self.board} on port {self.port}.')
             else:
                 # TODO: need to handle this case 
                 # return to main menu and flash message
                 print('Invalid Board. Create new or select different preset.')
+
         except Exception:
             # TODO: need to handle this case
             # return to main menu, flash message
@@ -44,19 +50,19 @@ class RunPreset:
 
     def establishPinConnection(self):
         global pins
-        for pin in self.pins:
+        for i, pin in enumerate(self.pins):
             try:
                 if pin[3] == 's':
-                    pin = board.get_pin(f'{pin[1]}:{pin[2]}:o')
-                    pin.mode = SERVO
+                    pins.append(board.get_pin(f'{pin[1]}:{pin[2]}:o'))
+                    pins[i].mode = SERVO
                 else:
-                    pin = board.get_pin(f'{pin[1]}:{pin[2]}:{pin[3]}')
+                    pins.append(board.get_pin(f'{pin[1]}:{pin[2]}:{pin[3]}'))
                     if pin[3] == 'p':
-                        pin.mode = PWM
+                        pin[i].mode = PWM
                     elif pin[3] == 'o':
-                        pin.mode = OUTPUT
+                        pins[i].mode = OUTPUT
                     else:
-                        pin.mode = INPUT
+                        pins[i].mode = INPUT
                 print(f'Established connection to pin {pin[2]}.')
             except Exception:
                 # TODO: need to handle this case
@@ -64,3 +70,37 @@ class RunPreset:
                 print(f'Cannot connect to pin {pin[2]}.')
                 print('Make a new preset or switch your connections around to the board.')
 
+    def operatePreset(self):
+        global pins
+        global board
+
+        it = pyfirmata.util.Iterator(board)
+        it.start()
+
+        while True:
+            event = keyboard.read_event()
+            for i, pin in enumerate(pins):
+                if event.event_type == keyboard.KEY_DOWN and event.name == self.pins[i][6]:
+                    # TODO: add functionality for positive key pressed
+                    if self.pin[i][3] == 's': # Pin is 'Servo'
+                        # TODO: add generic step size to current servo position
+                        pass
+                    elif self.pin[i][3] == 'o': # Pin is Output
+                        # TODO: write a 1 to the output pin to turn on
+                        pass
+                    elif self.pin[i][3] == 'i': # Pin is 'Input'
+                        pass
+                    else: # Pin is 'PWM'
+                        pass
+                elif event.event_type == keyboard.KEY_DOWN and event.name == self.pins[i][7]:
+                    # TODO: add functionality for negative key pressed
+                    if self.pin[i][3] == 's': # Pin is 'Servo'
+                        # TODO: subtract generic step size to current servo position
+                        pass
+                    elif self.pin[i][3] == 'o': # Pin is Output
+                        # TODO: write a 0 to the output pin to turn off
+                        pass
+                    elif self.pin[i][3] == 'i': # Pin is 'Input'
+                        pass
+                    else: # Pin is 'PWM'
+                        pass
